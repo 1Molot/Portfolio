d31a999bf32aaa92f4c1b544c9f38a2473fc0e31
1Molot <sinkevich1990@gmail.com>
2023-06-25 17:03:47 +0300

    fix:main:animaciia-partical-biblioteka

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,46 +1,116 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import style from './Main.module.css';
 import styleContainer from './../common/styles/Container.module.css'
 import myPhoto from "../assets/img/myPhoto.jpg";
+import { loadFull } from "tsparticles";
+import { Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+
+
 
 export const MainInform = () => {
 
-    const name = () => {
-        let text = 'I am Maksim Sinkevich Front-end Developer'
-        let content = document.querySelector('#content')
-        // @ts-ignore
-        for (let i in [...text]) {
-            let letter = document.createElement('span')
-            letter.classList.add('animated-span')
+        const particlesInit = useCallback(async (engine: Engine) => {
+            console.log(engine);
+            await loadFull(engine);
+        }, []);
+
+        const particlesLoaded = useCallback(async (container: any) => {
+            await console.log(container);
+        }, []);
+
+        const name = () => {
+            let text = 'I am Maksim Sinkevich Front-end Developer'
+            let content = document.querySelector('#content')
             // @ts-ignore
-            letter.textContent = [...text][i]
-            // @ts-ignore
-            if (letter.textContent.match(/\s/)) {
+            for (let i in [...text]) {
+                let letter = document.createElement('span')
+                letter.classList.add('animated-span')
+                // @ts-ignore
+                letter.textContent = [...text][i]
+                // @ts-ignore
+                if (letter.textContent.match(/\s/)) {
+                }
+                // @ts-ignore
+                letter.style.animationDelay = i / 10 + 's'
+                // @ts-ignore
+                content?.appendChild && content?.appendChild(letter)
             }
-            // @ts-ignore
-            letter.style.animationDelay = i / 10 + 's'
-            // @ts-ignore
-            content?.appendChild && content?.appendChild(letter)
         }
+
+        useEffect(() => {
+            name()
+        }, [])
+
+
+        return (
+            <section className={style.mainBlock}>
+                <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={{
+                        fpsLimit: 60,
+                        interactivity: {
+                            events: {
+                                resize: true,
+                            },
+                        },
+                        particles: {
+                            color: {
+                                value: "#ffa500",
+                            },
+                            links: {
+                                color: "#ffffff",
+                                distance: 150,
+                                enable: true,
+                                opacity: 0.3,
+                                width: 1,
+                            },
+                            collisions: {
+                                enable: true,
+                            },
+                            move: {
+                                direction: "none",
+                                enable: true,
+                                outModes: {
+                                    default: "bounce",
+                                },
+                                random: false,
+                                speed: 1,
+                                straight: false,
+                            },
+                            number: {
+                                density: {
+                                    enable: true,
+                                    area: 800,
+                                },
+                                value: 80,
+                            },
+                            opacity: {
+                                value: 0.1,
+                            },
+                            shape: {
+                                type: "circle",
+                            },
+                            size: {
+                                value: {min: 1, max: 5},
+                            },
+                        },
+                        detectRetina: true,
+                    }}
+                />
+                <div className={`${style.mainContainer} ${styleContainer.container}`}>
+                    <div className={style.text}>
+                        <h2 className={style.h2Text}>Hi There</h2>
+                        <div id="content"/>
+                    </div>
+                    <div className={style.photo}>
+                        <img src={myPhoto} alt={'myPhoto'}>
+                        </img>
+                    </div>
+
+                </div>
+            </section>
+        )
     }
-
-    useEffect(() => {
-        name()
-    }, [])
-
-    return (
-        <section className={style.mainBlock}>
-            <div className={`${style.mainContainer} ${styleContainer.container}`}>
-                <div className={style.text}>
-                    <h2 className={style.h2Text}>Hi There</h2>
-                    <div id="content"/>
-                </div>
-                <div className={style.photo}>
-                    <img src={myPhoto} alt={'myPhoto'}>
-                    </img>
-                </div>
-
-            </div>
-        </section>
-    )
-}
