b66544936a1d85f96918e3b336dd65d9b3dfaa28
1Molot <sinkevich1990@gmail.com>
2023-06-26 18:23:22 +0300

    fix:fix-biblioteki

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -22,9 +22,9 @@ export const MainInform = () => {
 
     const defaultOptions = {
         reverse: false,  // reverse the tilt direction
-        max: 35,     // max tilt rotation (degrees)
-        perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
-        scale: 1.1,    // 2 = 200%, 1.5 = 150%, etc..
+        max: 20,     // max tilt rotation (degrees)
+        perspective: 1000,   // Transform perspective, the //lower the more extreme the tilt gets.
+        scale: 1.03,    // 2 = 200%, 1.5 = 150%, etc..
         speed: 1000,   // Speed of the enter/exit transition
         transition: true,   // Set a transition on enter/exit.
         axis: null,   // What axis should be disabled. Can be X or Y.
@@ -58,74 +58,75 @@ export const MainInform = () => {
 
     return (
         <section className={style.mainBlock}>
-            <Fade direction={"top-left"}>
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    fpsLimit: 60,
+                    interactivity: {
+                        events: {
+                            resize: true,
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffa500",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.3,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 1,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.1,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: {min: 1, max: 5},
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
                 <div className={`${style.mainContainer} ${styleContainer.container}`}>
-                    <Particles
-                        id="tsparticles"
-                        init={particlesInit}
-                        loaded={particlesLoaded}
-                        options={{
-                            fpsLimit: 60,
-                            interactivity: {
-                                events: {
-                                    resize: true,
-                                },
-                            },
-                            particles: {
-                                color: {
-                                    value: "#ffa500",
-                                },
-                                links: {
-                                    color: "#ffffff",
-                                    distance: 150,
-                                    enable: true,
-                                    opacity: 0.3,
-                                    width: 1,
-                                },
-                                collisions: {
-                                    enable: true,
-                                },
-                                move: {
-                                    direction: "none",
-                                    enable: true,
-                                    outModes: {
-                                        default: "bounce",
-                                    },
-                                    random: false,
-                                    speed: 1,
-                                    straight: false,
-                                },
-                                number: {
-                                    density: {
-                                        enable: true,
-                                        area: 800,
-                                    },
-                                    value: 80,
-                                },
-                                opacity: {
-                                    value: 0.1,
-                                },
-                                shape: {
-                                    type: "circle",
-                                },
-                                size: {
-                                    value: {min: 1, max: 5},
-                                },
-                            },
-                            detectRetina: true,
-                        }}
-                    />
+                    <Fade direction={"top-left"}>
+
                     <div className={style.text}>
                         <h2 className={style.h2Text}>Hi There</h2>
                         <div id="content"/>
                     </div>
-                    <Tilt options={defaultOptions} style={{marginTop: '60px'}}>
+                    <Tilt options={defaultOptions}>
                         <div className={style.photo}>
                             <img src={myPhoto} alt={'myPhoto'}/>
                         </div>
                     </Tilt>
+                    </Fade>
                 </div>
-            </Fade>
         </section>
     )
 }
